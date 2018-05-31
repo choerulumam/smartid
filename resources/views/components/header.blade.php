0b5b78b180781c960a691ac5f468ce81919c35dc
choerulumam <choerulumam25@gmail.com>
2018-05-31 16:37:21 +0700

    version 1

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,7 +3,12 @@
         <a class="sidebar-toggle" href="#" data-toggle="offcanvas"></a>
         <div class="navbar-custom-menu">
             <ul class="top-nav">
-                <li><a {{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-sign-out fa-lg"></i></a></li>
+                <li>
+                    <a href="{{ route('logout') }}" 
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fa fa-sign-out fa-lg"></i>
+                    </a>
+                </li>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 	{{ csrf_field() }}
                  </form>
